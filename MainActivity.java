667ae9adcb0aed068e36ea5aec66addaf5a6a497
Leo-Albert <115719692+Leo-Albert@users.noreply.github.com>
2022-10-13 21:24:08 +0530

    standard calculator in java as android project

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.leo.standardcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leo.standardcalculator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
